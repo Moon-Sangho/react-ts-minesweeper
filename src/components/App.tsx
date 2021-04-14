42c348583913cc8f06b900ce495d4ea176127bc4
Moon-Sangho <skym887@gmail.com>
2021-04-14 15:36:59 +0900

    feat: 마우스 이벤트(마우스 클릭 시 이모지 상태 변화) 추가

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,30 @@
-import { ReactNode, useState } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import NumberDisplay from 'components/NumberDisplay';
 import Button from 'components/Button';
 import { generateCells } from 'utils';
+import { Face } from 'types';
 import 'components/App.scss';
 
 const App = () => {
   const [cells, setCells] = useState(generateCells());
+  const [face, setFace] = useState(Face.default);
 
-  console.log('cells', cells);
+  useEffect(() => {
+    const handleMouseDown = () => {
+      setFace(Face.oh);
+    };
+
+    const handleMouseUp = () => {
+      setFace(Face.default);
+    };
+    window.addEventListener('mousedown', handleMouseDown);
+    window.addEventListener('mouseup', handleMouseUp);
+
+    return () => {
+      window.removeEventListener('mousedown', handleMouseDown);
+      window.removeEventListener('mouseup', handleMouseUp);
+    };
+  }, []);
 
   const renderCelles = (): ReactNode => {
     return cells.map((row, rowIndex) =>
@@ -23,13 +40,15 @@ const App = () => {
     );
   };
 
+  console.log('cells', cells);
+
   return (
     <div className="App">
       <div className="Header">
         <NumberDisplay value={0} />
         <div className="Face">
           <span role="img" aria-label="face">
-            🙂
+            {face}
           </span>
         </div>
         <NumberDisplay value={23} />
